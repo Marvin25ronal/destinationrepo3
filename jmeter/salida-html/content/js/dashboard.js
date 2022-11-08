7205961b7ae85ef4e034883e90b27f73d921ca56
Marvin Martínez <marvin1ronal@gmail.com>
2022-11-08 15:57:48 -0600

    Update master at 11/8/2022 3:57:46 PM

--- jmeter/salida-html/content/js/dashboard.js
+++ jmeter/salida-html/content/js/dashboard.js
@@ -173,7 +173,7 @@ $(document).ready(function() {
     });
 
     // Creates APDEX table
-    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.5, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.5, 500, 1500, "Obtener 1k pokemones"], "isController": false}]}, function(index, item){
+    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.0, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.0, 500, 1500, "Obtener 1k pokemones"], "isController": false}]}, function(index, item){
         switch(index){
             case 0:
                 item = item.toFixed(3);
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 0, 0.0, 605.0, 605, 605, 605.0, 605.0, 605.0, 605.0, 1.6528925619834711, 110.97946797520662, 0.22275309917355374], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Obtener 1k pokemones", 1, 0, 0.0, 605.0, 605, 605, 605.0, 605.0, 605.0, 605.0, 1.6528925619834711, 110.97946797520662, 0.22275309917355374], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 0, 0.0, 3411.0, 3411, 3411, 3411.0, 3411.0, 3411.0, 3411.0, 0.2931691586045148, 19.682987943418354, 0.039509124890061566], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Obtener 1k pokemones", 1, 0, 0.0, 3411.0, 3411, 3411, 3411.0, 3411.0, 3411.0, 3411.0, 0.2931691586045148, 19.682987943418354, 0.039509124890061566], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
--- solosonar/sonar.js
+++ solosonar/sonar.js
@@ -8,9 +8,8 @@ scanner(
             'sonar.projectVersion': '1.0.0',
             'sonar.projectDescription': 'Backend test qa nodejs',
             'sonar.sources': '../solucredit-app/src',
-            'sonar.tests': 'test',
             'sonar.login': 'd9618251fc7ab20cb7461e22831ddde5ca2050bc',
-            'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+            'sonar.baseDir': '../solucredit-app',
         }
     },
     () => process.exit()
