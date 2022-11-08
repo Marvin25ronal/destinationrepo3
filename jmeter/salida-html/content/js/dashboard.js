9a303114e7c2eca5f1f1d9b13e1011e448997903
Marvin Martínez <marvin1ronal@gmail.com>
2022-11-08 10:45:51 -0600

    Update master at 11/8/2022 10:45:50 AM

--- jmeter/salida-html/content/js/dashboard.js
+++ jmeter/salida-html/content/js/dashboard.js
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 0, 0.0, 657.0, 657, 657, 657.0, 657.0, 657.0, 657.0, 1.5220700152207, 102.21354166666666, 0.20512271689497716], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Obtener 1k pokemones", 1, 0, 0.0, 657.0, 657, 657, 657.0, 657.0, 657.0, 657.0, 1.5220700152207, 102.21354166666666, 0.20512271689497716], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 1, 0, 0.0, 1212.0, 1212, 1212, 1212.0, 1212.0, 1212.0, 1212.0, 0.8250825082508251, 55.406224216171616, 0.1111927599009901], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Obtener 1k pokemones", 1, 0, 0.0, 1212.0, 1212, 1212, 1212.0, 1212.0, 1212.0, 1212.0, 0.8250825082508251, 55.406224216171616, 0.1111927599009901], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
